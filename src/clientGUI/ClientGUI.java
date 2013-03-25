837b4814a91586f8fbdcb9aeac3d1f01d087e503
Sam <Sam@Sam-PC.home>
2013-03-25 23:50:35 +0000

    Basic GUI setup completed.

--- src/clientGUI/ClientGUI.java
+++ src/clientGUI/ClientGUI.java
@@ -9,8 +9,9 @@ import javax.swing.*;
 
 public class ClientGUI extends JFrame {
     
-    MainPanel mainPanel;
-    JTextArea informationTA;
+    OptionPanel optionPanel;
+    VotePanel votePanel;
+    InfoPanel infoPanel;
     
     public ClientGUI(String title) {
         super(title);
@@ -21,14 +22,14 @@ public class ClientGUI extends JFrame {
         
         setVisible(true);
         
-        mainPanel = new MainPanel(getAvailableWidth(), getAvailableHeight());
-        informationTA = new JTextArea("Wibble");
+        optionPanel = new OptionPanel(getAvailableWidth(), getAvailableHeight() / 3);
+        votePanel = new VotePanel(getAvailableWidth(), getAvailableHeight() / 3);
+        infoPanel = new InfoPanel(getAvailableWidth(), getAvailableHeight() / 3);
         
-        mainPanel.add(informationTA);
-        
-        add(mainPanel, BorderLayout.SOUTH);        
+        add(optionPanel, BorderLayout.NORTH);
+        add(votePanel, BorderLayout.CENTER);
+        add(infoPanel, BorderLayout.SOUTH);        
     }
-    
     
     /**
      * These two methods should only be called after the frame is visible.
@@ -44,18 +45,98 @@ public class ClientGUI extends JFrame {
         return getHeight() - getInsets().top - getInsets().bottom;
     }
     
+        /**
+     * Inner class to hold the option button area.
+     */
+    private class OptionPanel extends JPanel {
+        
+        JButton connectButton;
+        JButton getVotesButton;
+        JButton submitButton;
+        JButton quitButton;
+        
+        public OptionPanel(int width, int height) {
+            setSize(width, height);
+            setLayout(new GridLayout(1,4));      
+            
+            connectButton = new JButton("Connect");
+            getVotesButton = new JButton("Get Votes");
+            submitButton = new JButton("Submit");
+            quitButton = new JButton("Quit");
+            
+            add(connectButton);
+            add(getVotesButton);
+            add(submitButton);
+            add(quitButton);
+        }
+      
+        /**
+         * This method is invoked automatically when repaint occurs in
+         * the outer container
+         */
+        public void paintComponent(Graphics g)
+        {
+            super.paintComponent(g);
+        }
+    }
+        
     /**
+     * Inner class to hold the voting area.
+     */
+    private class VotePanel extends JPanel {
+        
+        JList voteList;
+        JList responseList;
+        String[] testList = new String[]{"one","two","three","four"};           //REMOVE
+        
+        public VotePanel(int width, int height) {
+            setSize(width, height);
+            setLayout(new GridLayout(1,2));
+            
+            voteList = new JList(testList);                                     //REMOVE TESTLIST
+            responseList = new JList(testList);                                 //REMOVE TESTLIST
+
+            voteList.setBorder(BorderFactory.createEtchedBorder());
+            responseList.setBorder(BorderFactory.createEtchedBorder());
+            
+            add(voteList);
+            add(responseList);
+        }
+      
+        /**
+         * This method is invoked automatically when repaint occurs in
+         * the outer container
+         */
+        public void paintComponent(Graphics g)
+        {
+            super.paintComponent(g);
+        }
+    }
+    
+        /**
      * Inner class describing the main panel.
      */
-    private class MainPanel extends JPanel
-    {
+    private class InfoPanel extends JPanel {
+        
         JTextArea textArea;
         
-        public MainPanel(int width, int height) // given
-        {
+        public InfoPanel(int width, int height) {
             setSize(width, height);
+            setLayout(new GridLayout(1,1));
+            
+            textArea = new JTextArea("Welcome to the Colaboration and "
+                                     + "Voting Environment\n", 10, 1);
+            JScrollPane scroll = new JScrollPane(textArea);
+            textArea.setEditable(false);
+            
+            add(scroll);
         }
 
+        public void setText(String text) {
+            textArea.setText(text);
+            textArea.repaint();
+        }
+        
         /**
          * This method is invoked automatically when repaint occurs in
          * the outer container
